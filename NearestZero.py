7852c94b341e166da271c18d407d22f75e79df3c
TheBatOn2021 <93090111+TheBatOn2021@users.noreply.github.com>
2022-04-05 21:26:41 +0300

    Sprint 11 Review 4

--- NearestZero.py
+++ NearestZero.py
@@ -1,27 +1,27 @@
-# ID 66701646
-from collections import Counter
-
-
-def hands_dex(k, matrix):
-    # Вот тут мы сделали "список частот"
-    cnt = Counter(matrix)
-    cnt = list(cnt.values())
-    counter = 0
-    [counter := counter + 1 for i in range(
-        len(cnt)) if (cnt[i] > 0 and cnt[i] <= k * 2)]
-    return counter
-
-
 def read_input():
-    k = int(input())
-    matrix = []
-    for i in range(4):
-        matrix.append(input())
-    matrix = "".join(map(str, matrix))
-    matrix = "".join(c for c in matrix if c.isdecimal())
-    return (k, matrix)
+    num = int(input())
+    plots = [c for c in input().split()]
+    return num, plots
+
+
+def nearest_zero(num: int, plots: list):
+    free_plot = (num * -1)-1
+    left_plot = [0] * num
+    min_dist = [0] * num
+    for count, value in enumerate(plots):
+        if value == '0':
+            free_plot = count
+        else:
+            left_plot[count] = count - free_plot
+    free_plot = float("inf")
+    for count, value in reversed(list(enumerate(plots))):
+        if value == '0':
+            free_plot = count
+        else:
+            min_dist[count] = min(free_plot - count, left_plot[count])
+    return min_dist
 
 
 if __name__ == "__main__":
-    k, matrix = read_input()
-    print(hands_dex(k, matrix))
+    num, plots = read_input()
+    print(*nearest_zero(num, plots))
